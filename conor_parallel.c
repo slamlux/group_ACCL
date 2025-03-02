f083911b7b2fde92ada30f4985cbb47c488d404a
ConorDarcy25 <154520111+ConorDarcy25@users.noreply.github.com>
2025-03-02 20:18:13 +0000

    Update conor_parallel.c

--- conor_parallel.c
+++ conor_parallel.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <omp.h>
+#include <mpi.h>
 
 void initialise_vector(double vector[], int size, double initial);
 double T_equilibrium_global(int T0, int T1, int T2);
@@ -9,11 +9,16 @@ double T_equilibrium(double T0, double T1);
 double T_time(double T_eq, double T0, double step_size);
 int check_args(int argc, char **argv);
 void print_header(FILE** p_out_file, int points);
-void update_temperatures(double* temperatures, int points, double time_step);
+void update_temperatures(double* temperatures, int points, double time_step, int rank, int size);
 int generate_timestamps(double* timestamps, int time_steps, double step_size);
 
 int main(int argc, char **argv)
 {
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     int points = check_args(argc, argv);
     double T0 = 300, T1 = 270, T2 = 270;
     int time_steps = 20;
@@ -28,36 +33,35 @@ int main(int argc, char **argv)
     initialise_vector(temperatures, points, 270.0);
     temperatures[0] = T0;
 
-    FILE* out_file = fopen("data/temperatures_over_time.csv", "w");
-    print_header(&out_file, points);
-
-    fprintf(out_file, "%d, %lf", 0, time_stamps[0]);
-    for (int j = 0; j < points; j++)
-    {
-        fprintf(out_file, ", %lf", temperatures[j]);
+    FILE* out_file = NULL;
+    if (rank == 0) {
+        out_file = fopen("data/temperatures_over_time.csv", "w");
+        print_header(&out_file, points);
     }
-    fprintf(out_file, "\n");
 
-    for (int i = 1; i < time_steps; i++)
+    for (int i = 0; i < time_steps; i++)
     {
-        update_temperatures(temperatures, points, step_size);
-        fprintf(out_file, "%d, %lf", i, time_stamps[i]);
-        for (int j = 0; j < points; j++)
+        update_temperatures(temperatures, points, step_size, rank, size);
+        if (rank == 0)
         {
-            fprintf(out_file, ", %lf", temperatures[j]);
+            fprintf(out_file, "%d, %lf", i, time_stamps[i]);
+            for (int j = 0; j < points; j++)
+            {
+                fprintf(out_file, ", %lf", temperatures[j]);
+            }
+            fprintf(out_file, "\n");
         }
-        fprintf(out_file, "\n");
     }
 
     free(time_stamps);
     free(temperatures);
-    fclose(out_file);
+    if (rank == 0) fclose(out_file);
+    MPI_Finalize();
     return 0;
 }
 
 void initialise_vector(double vector[], int size, double initial)
 {
-    #pragma omp parallel for
     for (int i = 0; i < size; i++)
     {
         vector[i] = initial;
@@ -103,35 +107,31 @@ void print_header(FILE** p_out_file, int points)
     fprintf(*p_out_file, "\n");
 }
 
-void update_temperatures(double* temperatures, int points, double time_step)
+void update_temperatures(double* temperatures, int points, double time_step, int rank, int size)
 {
+    int local_size = points / size;
+    int start = rank * local_size;
+    int end = (rank == size - 1) ? points : start + local_size;
+
     double* new_temperatures = (double*) malloc(points * sizeof(double));
     
-    #pragma omp parallel for
-    for (int i = 1; i < points - 1; i++)
+    for (int i = start; i < end; i++)
     {
-        double T_eq1 = T_equilibrium(temperatures[i], temperatures[i + 1]);
-        double T_eq2 = T_equilibrium(temperatures[i], temperatures[i - 1]);
-        double T1 = T_time(T_eq1, temperatures[i], time_step);
-        double T2 = T_time(T_eq2, temperatures[i], time_step);
-        new_temperatures[i] = T1 + T2 - temperatures[i];
+        if (i > 0 && i < points - 1) {
+            double T_eq1 = T_equilibrium(temperatures[i], temperatures[i + 1]);
+            double T_eq2 = T_equilibrium(temperatures[i], temperatures[i - 1]);
+            double T1 = T_time(T_eq1, temperatures[i], time_step);
+            double T2 = T_time(T_eq2, temperatures[i], time_step);
+            new_temperatures[i] = T1 + T2 - temperatures[i];
+        }
     }
 
-    new_temperatures[0] = T_time(T_equilibrium(temperatures[0], temperatures[1]), temperatures[0], time_step);
-    new_temperatures[points - 1] = T_time(T_equilibrium(temperatures[points - 2], temperatures[points - 1]), temperatures[points - 1], time_step);
-
-    #pragma omp parallel for
-    for (int i = 0; i < points; i++)
-    {
-        temperatures[i] = new_temperatures[i];
-    }
-
+    MPI_Allgather(new_temperatures + start, local_size, MPI_DOUBLE, temperatures, local_size, MPI_DOUBLE, MPI_COMM_WORLD);
     free(new_temperatures);
 }
 
 int generate_timestamps(double* timestamps, int time_steps, double step_size)
 {
-    #pragma omp parallel for
     for (int i = 0; i < time_steps; i++)
     {
         timestamps[i] = i * step_size;
